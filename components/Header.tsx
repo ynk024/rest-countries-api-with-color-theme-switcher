d5aae31eb858bf4b8d2d766403e5725c2ac96fbe
yfoel <yannick.foelsing@otto.de>
2022-02-07 22:37:54 +0100

    Set up Header

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="intro">
+        <h1>Where in the world?</h1>
+        <div className="text-with-icon">Dark Mode</div>
+      </div>
+    </header>
+  );
+};
